f964ddc3bb853465f0b027fcb3315675b934187b
zawkindev <zawkindev@gmail.com>
2024-08-25 07:20:55 +0500

    feat: add handlers and mux

---
+++ handler/handler.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "welcome man!")
+}
+
+func BookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		http.Error(w, "book ID is not specified!", http.StatusBadRequest)
+		return
+	}
+
+	id := r.URL.Path[1:]
+
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintln(w, "book ID: ", id)
+	case http.MethodPut:
+		updateBook(w, r, id)
+	}
+}
+
+func BooksHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getBooks(w, r)
+	case http.MethodPost:
+		createBook(w, r)
+	}
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "list of books")
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("BOOK CREATED")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request, id string) {
+	fmt.Fprintln(w, "UPDATED book ID: ", id)
+}
--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"fmt"
+	h "library-management-api/handler"
 	"net/http"
 )
 
 func main() {
 	mainMux := http.NewServeMux()
-	mainMux.HandleFunc("/", homeHandler)
-	http.ListenAndServe(":8080", mainMux)
-}
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "welcome man!")
+	booksMux := http.NewServeMux()
+	booksMux.HandleFunc("/", h.BookHandler)
+
+	mainMux.HandleFunc("/", h.HomeHandler)
+	mainMux.HandleFunc("/books", h.BooksHandler)
+	mainMux.Handle("/books/", http.StripPrefix("/books", booksMux))
+
+	http.ListenAndServe(":8080", mainMux)
 }
