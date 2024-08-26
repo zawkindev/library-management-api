ce6b3215d1bfaee4ad3dd7fb40a0df5d8abfcac6
zawkindev <zawkindev@gmail.com>
2024-08-26 09:07:28 +0500

    fix: requrie ssl

--- database/database.go
+++ database/database.go
@@ -9,7 +9,7 @@ var DB *sql.DB
 
 func ConnectDB() {
 	var err error
-	DB, err = sql.Open("postgres", "user=postgres password=m dbname=library sslmodule=require")
+	DB, err = sql.Open("postgres", "user=postgres password=m dbname=library sslmode=require")
 	if err != nil {
 		log.Fatal("Unable to connect to the database: ", err)
 	}
