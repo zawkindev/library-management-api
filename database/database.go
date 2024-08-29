1f69fc29dcac0d7cd4556fef6d4ace4b2efd7310
zawkindev <zawkindev@gmail.com>
2024-08-29 15:16:08 +0500

    feat: use gorm

--- database/database.go
+++ database/database.go
@@ -1,20 +1,36 @@
 package database
 
 import (
-	"database/sql"
+	"library-management-api/model"
 	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-var DB *sql.DB
+var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
-	DB, err = sql.Open("postgres", "user=postgres password=m dbname=library sslmode=require")
+	dsn := "user=postgres password=m dbname=library sslmode=require"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Unable to connect to the database: ", err)
 	}
 
-	if err = DB.Ping(); err != nil {
-		log.Fatal("Database connection failed: ", err)
+	// Auto-migrate the Book model
+	DB.AutoMigrate(&model.Book{})
+
+	// Insert books into the database
+	books := []model.Book{
+		{Title: "The Fellowship of the Ring", Author: "J.R.R. Tolkien", Year: 1954, Genre: "Fantasy", ISBN: "978-0261103573"},
+		{Title: "The Two Towers", Author: "J.R.R. Tolkien", Year: 1954, Genre: "Fantasy", ISBN: "978-0261103580"},
+		{Title: "The Return of the King", Author: "J.R.R. Tolkien", Year: 1955, Genre: "Fantasy", ISBN: "978-0261103597"},
 	}
+
+	for _, book := range books {
+		DB.Create(&book)
+	}
+
+	log.Println("Books added successfully")
 }
--- model/model.go
+++ model/model.go
@@ -1,10 +1,21 @@
 package model
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Book struct {
-	ID     string `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	Year   string `json:"year"`
-	Genre  string `json:"genre"`
-	ISBN   string `json:"isbn"`
+	ID     string `gorm:"type:uuid;primaryKey"`
+	Title  string `gorm:"type:varchar(255);not null"`
+	Author string `gorm:"type:varchar(255);not null"`
+	Year   int    `gorm:"type:int"`
+	Genre  string `gorm:"type:varchar(100)"`
+	ISBN   string `gorm:"type:varchar(20);unique"`
+}
+
+// BeforeCreate is a GORM hook that is called before a record is inserted into the database.
+func (book *Book) BeforeCreate(tx *gorm.DB) (err error) {
+	book.ID = uuid.New().String() // Generate a new UUID for the ID
+	return
 }
